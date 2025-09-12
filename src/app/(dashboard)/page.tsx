38f2054f4ccf319245a508405bf020778fd69e96
the-shamsudeen <imuhammadshamsudeen@gmail.com>
2025-09-12 12:30:45 +0100

    agents setup

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,6 +1,6 @@
 import { auth } from "@/lib/auth";
 import { HomeView } from "@/modules/home/ui/views/home-view";
-import { caller } from "@/trpc/server";
+// import { caller } from "@/trpc/server";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,11 +1,5 @@
 "use client";
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-
 export const HomeView = () => {
-  const trpc = useTRPC();
-  const { data } = useQuery(trpc.hello.queryOptions({ text: "theshamsudeen" }));
-
-  return <div className="p-4 flex flex-col gap-y-4">{data?.greeting}</div>;
+  return <div>Home View</div>;
 };
